339b0cb50d4a77d099b637054c9967eb04d3d0f2
nilofernissar <145194565+nilofernissar@users.noreply.github.com>
2023-11-20 23:50:35 +0530

    Add files via upload

---
+++ start_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
+
+  @override
+  Widget build(context) {
+    return Center(
+        child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          'assets/images/quiz-logo.png',
+          width: 300,
+          color: Color.fromARGB(150, 255, 255, 255),
+        ),
+        const SizedBox(height: 80),
+        const Text(
+          'Learn Flutter the fun way!',
+          style: TextStyle(
+            color: Color.fromARGB(253, 12, 7, 59),
+            fontSize: 24,
+          ),
+        ),
+        const SizedBox(height: 30),
+        OutlinedButton.icon(
+          onPressed: startQuiz,
+          style: OutlinedButton.styleFrom(
+            foregroundColor: Colors.white,
+          ),
+          icon: Icon(Icons.arrow_right_alt),
+          label: const Text('Start Quiz'),
+        )
+      ],
+    ));
+  }
+}
